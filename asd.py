6ba471f7b8029b0e0dc76a7fc3366412b919ef65
kimtaeil12 <145953732+kimtaeil12@users.noreply.github.com>
2023-10-16 10:31:47 +0900

    Add files via upload

---
+++ asd.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+x = True
+y = False
+
+if x and y:
+    print("Yes")
+else:
+    print("No")
+
+
+# In[6]:
+
+
+a=[1,2,3,4]
+b=3 in a
+print(b)
+
+
+# In[8]:
+
+
+a = "ABC"
+b = c
+print(a is b)
+
